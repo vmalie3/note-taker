1079656ae03d7c7ff4a5de8c324744534d88825e
Victoria Malie <vmalie3@gmail.com>
2022-06-22 15:00:59 -0500

    added get and post requests to show existing and new notes

--- server.js
+++ server.js
@@ -1,13 +1,51 @@
 const express = require('express');
 const path = require('path');
 const port =  process.env.PORT || 3001;
+const fs = require('fs');
 
 const app = express();
 
+app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'public/index.html')));
 app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, '/public/notes.html')));
+
+app.get('/api/notes', (req, res) => {
+    let notes = JSON.parse(fs.readFileSync('./db/db.json', 'utf8', 2));
+
+    return res.json(notes);
+})
+
+app.post('/api/notes', (req, res) => {
+    let data = path.join(__dirname, '/db/db.json');
+    let newNote = req.body;
+
+    console.log(newNote);
+
+    let id = 1;
+    let arr = require('./db/db.json')
+
+    for (let i = 0; i < arr.length; i++) {
+        let thisNote = arr[i];
+        if (thisNote.id > id) {
+            id = thisNote.id;
+        }
+    }
+
+    newNote.id = id + 1;
+    arr.push(newNote);
+
+    fs.writeFile(data, JSON.stringify(arr), (err) => {
+        if (err) {
+            return console.log(err);
+        }
+    })
+
+    return res.json(newNote);
+})
+
+
 
 app.listen(port);
